86b2d50daef4c8673c60ce38f42d871a0191bd64
JugglerShu <shu@jugglershu.net>
2014-08-16 14:20:14 +0900

    Add LRUCache

--- JSLibTest.cpp
+++ JSLibTest.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+
 #include "include/algorithm"
+#include "include/LRUCache.h"
 #include <iterator>
 #include <vector>
 #include <algorithm>
@@ -31,7 +34,42 @@ void Test_transform_if(){
     TEST_ASSERT(arr2[2] == 15);
 }
 
+void Test_LRUCache(){
+    // insert
+    JSLib::LRUCache<std::string, int> myCache(3);
+    myCache.insert("1",1);
+    TEST_ASSERT(myCache.size() == 1);
+    myCache.insert("2",2);
+    TEST_ASSERT(myCache.size() == 2);
+    myCache.insert("3",3);
+    TEST_ASSERT(myCache.size() == 3);
+    myCache.insert("4",4); // 1st pair must be dropped out here
+    TEST_ASSERT(myCache.size() == 3);
+    TEST_ASSERT(myCache.get("1") == NULL);
+    TEST_ASSERT(*myCache.get("2") == 2);
+    TEST_ASSERT(*myCache.get("3") == 3);
+    TEST_ASSERT(*myCache.get("4") == 4);
+
+    // Must override the value
+    myCache.insert("2",5);
+    TEST_ASSERT(*myCache.get("2") == 5);
+
+    // LRU
+    JSLib::LRUCache<std::string, int> myCache2(3);
+    myCache2.insert("1",1);
+    myCache2.insert("2",2);
+    myCache2.insert("3",3);
+    myCache2.get("1"); // now 1 is the most recenly used entry
+    myCache2.insert("4",4); // "2"/2 pair must be dropped out here
+    TEST_ASSERT(myCache2.size() == 3);
+    TEST_ASSERT(*myCache2.get("1") == 1);
+    TEST_ASSERT(myCache2.get("2") == NULL);
+    TEST_ASSERT(*myCache2.get("3") == 3);
+    TEST_ASSERT(*myCache2.get("4") == 4);
+}
+
 int main(int argc, const char** argv){
     Test_transform_if();
+    Test_LRUCache();
     return 0;
 }
---
+++ include/LRUCache.h
@@ -0,0 +1,91 @@
+#ifndef __JSLIB_LRUCACHE_H__
+#define __JSLIB_LRUCACHE_H__
+
+#include <unordered_map>
+#include <list>
+#include <cassert>
+
+namespace JSLib{
+
+/**
+ * LRUCache
+ * LRUCache manages key/value pair in least recently used cahce manner.
+ * You can specify max entry size on construct.
+ * When you get a value from a key, the entry is moved to the top 
+ * of list as the most recently used entry.
+ * If you insert a key/value pair that of key already exists in the cache
+ * the existing entry is removed and new key/value pair is inserted
+ * as the most recently used entry.
+ * 
+ **/
+
+template<class KEY, class VALUE>
+class LRUCache{
+    struct ListEntry;
+    typedef std::list<ListEntry> ValueList;
+    typedef std::unordered_map<KEY, typename ValueList::iterator> KeyHashTable;
+
+    struct ListEntry{
+        ListEntry(VALUE v, typename KeyHashTable::iterator i):value(v),it(i){};
+        ~ListEntry(){};
+        VALUE value;
+        typename KeyHashTable::iterator it;
+    };
+
+public:
+    /**
+     * Constructor
+     * Specify the max entry size.
+     * If the size is 0 the max entry size is unlimited.
+     **/
+    LRUCache(size_t max_size):maxSize_(max_size){};
+    
+    void insert( const KEY& key, const VALUE& value ){
+        assert(values_.size() == keyMap_.size());
+        typename KeyHashTable::const_iterator existingValue = keyMap_.find(key);
+        if( existingValue != keyMap_.end()){
+            // Move the value to the front.
+            values_.splice(values_.begin(), values_, existingValue->second);
+            values_.begin()->value = value;
+            // No need to update the iterator in keyMap_ since the iterator is still valid.
+        }else{
+            if( 0 != maxSize_ && maxSize_ <= values_.size() ){
+                // Erase the least recently used entry
+                keyMap_.erase(values_.rbegin()->it);
+                values_.pop_back();
+            }
+            values_.push_front(ListEntry(value, keyMap_.begin())); // The iterator is temporary.
+            std::pair<typename KeyHashTable::iterator, bool> result = keyMap_.insert(std::make_pair(key,values_.begin()));
+            values_.begin()->it = result.first;
+        }
+    };
+
+    const VALUE* get(const KEY& key) {
+        assert(values_.size() == keyMap_.size());
+        typename KeyHashTable::const_iterator i = keyMap_.find(key);
+        if( i != keyMap_.end() ){
+            values_.splice(values_.begin(), values_, i->second);
+            return &(i->second->value);
+        }else{
+            return reinterpret_cast<VALUE*>(NULL);
+        }
+    };
+
+    void clear(){
+        assert(values_.size() == keyMap_.size());
+        values_.clear();
+        keyMap_.clear();
+    };
+
+    size_t size(){
+        assert(values_.size() == keyMap_.size());
+        return values_.size(); 
+    }
+private:
+    size_t maxSize_;
+    KeyHashTable keyMap_;
+    ValueList values_;
+};
+
+} // JSLib
+#endif
